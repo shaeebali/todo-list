9930cebdf129c739d96a7df00224fdceef6b66dc
shaeeb <shaeebali@gmail.com>
2024-05-16 12:44:06 +0100

    export routes

--- routes/todo.js
+++ routes/todo.js
@@ -1,3 +1,9 @@
 const express = require('express');
-import { create, get, remove } from ('../model/todo');
+import { create, get, removeTodo } from ('../controller/index.js');
 const router = express.Router();
+
+router.post('/todo/create', create);
+router.get('/todos', get);
+router.delete('/todo/:id', removeTodo);
+
+module.exports = router;
